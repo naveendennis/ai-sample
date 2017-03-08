6bf03594e37a2b10d2ff9f35b8c3a1d78b63b8a5
Naveen Dennis <naveendennis@gmail.com>
2017-03-07 19:13:31 -0500

    adding shuffling to neural network

--- utils/amazon_utils.py
+++ utils/amazon_utils.py
@@ -1,5 +1,3 @@
-
-import numpy as np
 import nltk
 import re
 import pickle
@@ -53,6 +51,7 @@ def get_label_indices(label_list):
         label_val = next_char(label_val)
 
     return r_indices
+
 
 def get_duplicate_list(vocabulary_list):
     """
@@ -263,6 +262,7 @@ def train_test_split(feature_list, label_list):
         feature_test.append(feature_list[index])
 
     return feature_train, feature_test, label_train, label_test
+
 
 def shuffle(feature, label):
     size = len(label)
